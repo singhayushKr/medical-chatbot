43231537743c1a204b09abe19b2d6bdfe35013fe
Itsdevilshivay <91064207+Itsdevilshivay@users.noreply.github.com>
2024-03-12 20:37:10 +0530

    Add files via upload

---
+++ End-to-end-Medical-Chatbot-using-Llama2-main/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Ayush kr singh',
+    author_email= 'ayushsingh4167@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
